7d6304f523917decfddb67721196504a4e8abb61
saida179 <69127945+saida179@users.noreply.github.com>
2021-09-26 01:21:35 -0700

    greating page

--- sa/src/App.js
+++ sa/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import 'bootswatch/dist/slate/bootstrap.min.css';
 import './App.css';
+import Footer from "./components/Footer";
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome!</h1>
+        <div class="card border-light mb-3">
+          <div class="card-body">
+            <h4 class="card-title">I'm Saida Arevalo,</h4>
+            <p class="card-text">here you can find information about my journey as a full stack developer.</p>
+          </div>
+        </div>
       </header>
+      <Footer />
     </div>
   );
 }
