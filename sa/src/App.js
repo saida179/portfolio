fc7adc7a3312a21fa465057f85a86cc95a6b3888
saida179 <69127945+saida179@users.noreply.github.com>
2021-09-28 18:46:51 -0700

    app.js changes

--- sa/src/App.js
+++ sa/src/App.js
@@ -1,6 +1,10 @@
 import 'bootswatch/dist/slate/bootstrap.min.css';
 import './App.css';
 import Footer from "./components/Footer";
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { SiGmail } from "react-icons/si";
+
 
 function App() {
 
@@ -14,6 +18,11 @@ function App() {
             <h4 class="card-title">I'm Saida Arevalo,</h4>
             <p class="card-text">here you can find information about my journey as a full stack developer.</p>
           </div>
+        </div>
+        <div>
+          <FaLinkedin />
+          <FaGithub />
+          <SiGmail />
         </div>
       </header>
       <Footer />
